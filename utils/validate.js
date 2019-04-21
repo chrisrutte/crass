3337ed3a53c04f6df2ec0aab572944a010e555ec
chrisrutte <chrisrutte@gmail.com>
2019-04-21 19:50:29 +0200

    extra validation options

--- utils/validate.js
+++ utils/validate.js
@@ -2,7 +2,7 @@ function validate(object, sheet) {
   
   if (sheet === 'customers') {
     if (!object.name) {throw 'Please provide a name'}
-    
+    if (object.name.length < 5) {throw 'Make sure your name is more than 4 characters'}
     object.name = object.name.trim()
   }
   
